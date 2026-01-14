f7b1f0464168eab7431ec83d653d82ab6529bb21
FredericHirschmueller <fh@dgs-berlin.de>
2026-01-14 17:30:27 +0100

    Fix component hydrogen demand

--- src/placades/facades/demand/hydrogen_demand.py
+++ src/placades/facades/demand/hydrogen_demand.py
@@ -1,47 +1,49 @@
-# from oemof.solph import Flow
-# from oemof.solph.components import Sink
-#
-#
-# class H2Demand(Sink):
-#     """
-#     Hydrogen demand/consumption component.
-#
-#     This class represents a hydrogen demand that consumes hydrogen
-#     according to a specified time series pattern.
-#
-#     :Structure:
-#       *input*
-#         1. from_bus : H2
-#
-#     Parameters
-#     ----------
-#     name : str
-#         Name of the asset.
-#     input_timeseries : str or None, default=None
-#         Name of the csv file containing the input generation or
-#         demand timeseries.
-#
-#     Examples
-#     --------
-#     >>> from oemof.solph import Bus
-#     >>> h2_bus = Bus(label="hydrogen_bus")
-#     >>> my_h2_demand = H2Demand(
-#     ...     name="fuel_cell_demand",
-#     ...     bus=h2_bus,
-#     ...     input_timeseries="hydrogen_demand.csv",
-#     ... )
-#
-#     """
-#
-#     def __init__(self, name, bus, input_timeseries):
-#         self.profile = input_timeseries
-#         self.name = name
-#         super().__init__(
-#             label=name,
-#             inputs={
-#                 bus: Flow(
-#                     fix=input_timeseries,
-#                     nominal_capacity=1,
-#                 )
-#             },
-#         )
+from oemof.solph import Flow
+from oemof.solph.components import Sink
+
+
+class H2Demand(Sink):
+    def __init__(self, name, bus_in_hydrogen, input_timeseries):
+        """
+        Hydrogen demand/consumption component.
+
+        This class represents a hydrogen demand that consumes hydrogen
+        according to a specified time series pattern.
+
+        :Structure:
+        *input*
+            1. from_bus : Hydrogen
+
+        Parameters
+        ----------
+        name : str
+            |name|
+        bus_in_hydrogen : placades.CarrierBus
+            |bus_in_hydrogen|
+        input_timeseries : array-like
+            |input_timeseries|
+
+        Examples
+        --------
+        >>> from placades import CarrierBus as Bus
+        >>> h2_bus = Bus(label="hydrogen_bus")
+        >>> my_h2_demand = H2Demand(
+        ...     name="fuel_cell_demand",
+        ...     bus_in_hydrogen=h2_bus,
+        ...     input_timeseries="hydrogen_demand.csv",
+        ... )
+
+        """
+
+        self.profile = input_timeseries
+        self.name = name
+
+        super().__init__(
+            label=name,
+            inputs={
+                bus_in_hydrogen: Flow(
+                    fix=input_timeseries,
+                    nominal_capacity=1,
+                )
+            },
+        )

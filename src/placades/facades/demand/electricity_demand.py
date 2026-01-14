a44a44645fd7425bc35c91abe4f2266d73ff795d
FredericHirschmueller <fh@dgs-berlin.de>
2026-01-14 14:43:14 +0100

    Fix component electricity demand

--- src/placades/facades/demand/electricity_demand.py
+++ src/placades/facades/demand/electricity_demand.py
@@ -3,37 +3,38 @@ from oemof.solph.components import Sink
 
 
 class Demand(Sink):
-    """
-    Electricity demand/consumption component.
-
-    This class represents an electricity demand that consumes
-    electrical energy according to a specified time series pattern.
-
-    :Structure:
-      *input*
-        1. from_bus : Electricity
-
-    Parameters
-    ----------
-    name : str
-        |name|
-    bus_in_electricity : placades.CarrierBus
-        |bus_in_electricity|
-    input_timeseries : array-like
-        |input_timeseries|
-
-    Examples
-    --------
-    >>> from placades import CarrierBus as Bus
-    >>> ebus = Bus(name="electricity_bus")
-    >>> my_demand = Demand(
-    ...     name="office_demand",
-    ...     bus_in_electricity=ebus,
-    ...     input_timeseries="electricity_demand.csv",
-    ... )
-    """
-
     def __init__(self, name, bus_in_electricity, input_timeseries):
+        """
+        Electricity demand/consumption component.
+
+        This class represents an electricity demand that consumes
+        electrical energy according to a specified time series pattern.
+
+        :Structure:
+        *input*
+            1. from_bus : Electricity
+
+        Parameters
+        ----------
+        name : str
+            |name|
+        bus_in_electricity : placades.CarrierBus
+            |bus_in_electricity|
+        input_timeseries : array-like
+            |input_timeseries|
+
+        Examples
+        --------
+        >>> from placades import CarrierBus as Bus
+        >>> ebus = Bus(name="electricity_bus")
+        >>> my_demand = Demand(
+        ...     name="office_demand",
+        ...     bus_in_electricity=ebus,
+        ...     input_timeseries="electricity_demand.csv",
+        ... )
+
+        """
+
         self.profile = input_timeseries
         self.name = name
 

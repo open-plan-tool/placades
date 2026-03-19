09313ccd3cc157fcf3f7088c8759472afdb2e936
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2026-03-19 22:47:30 +0100

    Add graph description

--- src/oemof/eesyplan/postprocessing/graphs.py
+++ src/oemof/eesyplan/postprocessing/graphs.py
@@ -252,3 +252,159 @@ def capacities_graph(
     )
 
     return fig
+
+
+def stacked_timeseries(flows, energy_system, scenario_name=None):
+    """
+      go through each busses of a different carrier type and flag the components belonging to the different carriers
+     flow coming into a "storage", "heat_pump", "chp_fixed_ratio" or "chp" are flagged as "demand" for the `group`
+     attribute of the stacked graph. Any component of type sink will also be flagged as "demand", all other components
+     will belong to the "production" group. The value into the `mode` attribute will be 'lines' for `group="demand"` and 'none'
+     The value into the `fill` attribute will be 'none' for the demand group and 'tonexty' for the production group
+     One needs also to sort the timeseries according to this rule: first demand, then storages and finally dsos, the
+     other types come then next
+     Here we shouldn't a priori see the flows internal to subnetworks
+
+    if scenario_name is provided, then the label of the traces should have it as a prefix (useful when producing a
+    comparison figure of several simulations)
+
+    """
+
+
+def graph_timeseries(
+    flows, energy_system, y_variables=None, scenario_name=None
+):
+    """
+    prepare a graph with all timeseries of the components of the energy_system with
+    a positive sign for the "production" (going into a bus) and a negative sign for "demand" (going outside a bus)
+
+    if y_variables is provided (list of strings), then only the component label within this list will be graphed
+
+    if scenario_name is provided, then the label of the traces should have it as a prefix (useful when producing a
+    comparison figure of several simulations)
+    """
+
+
+def graph_costs(arrangement, scenario_name=None):
+    """
+    Compute capex_total, opex_fix_total, opex_var_total, fuel_costs_total from the following information one need to
+    gather for each component:
+    Example:
+    >                         label  installed_capacity  capex_fix  capex_var  opex_fix  opex_var  lifetime energy_price   optimized_capacity     total_flow direction
+    0                        BHKW                 0.0        0.0     1100.0      11.0   0.00000        15            0     10.728721             60135.444842        in
+    1                  Gas_boiler                 0.0        0.0      120.0       4.0   0.00000        20            0     93.602946            159106.826600        in
+    2              Solarkollektor                 0.0        0.0      300.0      20.0   0.00000        20            0     3.872608               2800.789583        in
+
+    Maybe those calculation are already provided by the Results object and the first part of this fonction is obsolete
+
+    if scenario_name is provided, then the label of the traces should have it as a prefix (useful when producing a
+    comparison figure of several simulations)
+
+    """
+
+    # 4 variant of the cost graph, to be chosen by the user within arrangement argument
+    COSTS_PER_ASSETS = "var1"  # currently this one is plotted on the result page in OpenPlan GUI
+    COSTS_PER_CATEGORY = "var2"
+    COSTS_PER_CATEGORY_STACKED = "var3"
+    COSTS_PER_ASSETS_STACKED = "var4"
+
+    wacc = 0.1  # should come from `discount_factor` value within project.csv resource
+    df = pd.DataFrame()  # should contain the above
+
+    def annualize_capex(capex, wacc, lifetime):
+        ann_capex = (
+            capex
+            * (wacc * (1 + wacc) ** lifetime)
+            / ((1 + wacc) ** lifetime - 1)
+        )
+        return ann_capex
+
+    # TODO costs for batteries are skewed as battery capacity does not exists in fancy results
+    # TODO costs for dso not implemented yet
+    df["capex_total"] = df.apply(
+        lambda x: annualize_capex(
+            ((x.installed_capacity + x.optimized_capacity) * x.capex_var),
+            wacc,
+            x.lifetime,
+        ),
+        axis=1,
+    )
+
+    df["opex_fix_total"] = df.apply(
+        lambda x: (x.installed_capacity + x.optimized_capacity) * x.opex_fix,
+        axis=1,
+    )
+    df["opex_var_total"] = df.apply(
+        lambda x: x.total_flow * x.opex_var, axis=1
+    )
+
+    # nur für dso ...
+    df["fuel_costs_total"] = df.apply(
+        lambda x: x.total_flow * x.energy_price, axis=1
+    )
+
+    if arrangement in [COSTS_PER_ASSETS_STACKED, COSTS_PER_CATEGORY_STACKED]:
+        x_values = [
+            # x
+            # for x in Scenario.objects.filter(simulation__in=simulations).values_list(
+            #     "name", flat=True
+            # )
+        ]  # need to be understood from OpenPlan context
+        y_values = []
+
+    # This is the current arrangement within OpenPlan GUI
+    if arrangement == COSTS_PER_ASSETS:
+        x_values = df.index.values.tolist()
+        y_values = []
+        for i in range(len(df.columns)):
+            name = df.iloc[:, i].name.replace("_total", "")
+            y = df.iloc[:, i].values.tolist()
+            y_values.append(
+                {
+                    "base": (
+                        df.iloc[:, :i].sum(axis=1).values.tolist()
+                        if i > 0
+                        else None
+                    ),
+                    "value": y,
+                    "text": [name for j in range(len(x_values))],
+                    "name": (
+                        name
+                        if scenario_name is None
+                        else f"{name} {scenario_name}"
+                    ),
+                    "hover": "<b>%{text}, </b><br><br>Block value: %{customdata:.2f}$<br>Stacked value: %{y:.2f}$<extra> %{x}</extra>",
+                    "customdata": y,
+                    # https://stackoverflow.com/questions/59057881/python-plotly-how-to-customize-hover-template-on-with-what-information-to-show
+                }
+            )
+
+    elif arrangement == COSTS_PER_CATEGORY:
+        x_values = df.columns.tolist()
+        y_values = []
+        for i in range(len(df.index)):
+            name = df.iloc[i, :].name
+            y = df.iloc[i, :].values.tolist()
+            y_values.append(
+                {
+                    "base": (
+                        df.iloc[:i, :].sum(axis=0).values.tolist()
+                        if i > 0
+                        else None
+                    ),
+                    "value": y,
+                    "text": [name for j in range(len(x_values))],
+                    "name": (
+                        name
+                        if scenario_name is None
+                        else f"{name} {scenario_name}"
+                    ),
+                    "hover": "<b>%{text}</b><br><br>Block value: %{customdata:.2f}$<br>Stacked value: %{y:.2f}$",
+                    "customdata": y,
+                }
+            )
+
+    elif arrangement == COSTS_PER_CATEGORY_STACKED:
+        y_values.append(df.sum(axis=1))
+    elif arrangement == COSTS_PER_ASSETS_STACKED:
+        y_values.append(df.sum(axis=0))

46a2f10f968a4952d6b408356c71d08c5fe662fa
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-06-12 17:34:29 +0200

    Add excess and shortage to CarrierBus

--- src/oemof/eesyplan/components/buses/carrier.py
+++ src/oemof/eesyplan/components/buses/carrier.py
@@ -1,11 +1,19 @@
 from oemof.solph import Bus
+from oemof.solph import Flow
+from oemof.solph.components import Sink
+from oemof.solph.components import Source
 
 
 class CarrierBus(Bus):  # todo add shortage source and excess sink with costs
     """Bus mit Medium-Attribut"""
 
     def __init__(
-        self, name, carrier=None, balanced=None, excess=None, excess_costs=None
+        self,
+        name,
+        carrier=None,
+        balanced=True,
+        excess_cost=None,
+        shortage_cost=None,
     ):
         """
         Bus mit Energieträger-Information
@@ -17,21 +25,57 @@ class CarrierBus(Bus):  # todo add shortage source and excess sink with costs
         carrier : str
             Energieträger/Medium (z.B. 'electricity', 'gas', 'heat',
             'hydrogen')
-        **kwargs
-            Weitere Parameter für Bus
+        balanced : bool
+            Text
+        excess_cost : float
+            Text
+        shortage_cost: float
+            Text
 
         Examples
         --------
-        >>> electricity_bus = CarrierBus(name="grid", carrier="electricity")
-        >>> gas_bus = CarrierBus(name="gas_grid", carrier="natural_gas")
-        >>> heat_bus = CarrierBus(name="district_heating", carrier="heat")
-        >>> h2_bus = CarrierBus(name="h2_network", carrier="hydrogen")
+        >>> electricity_bus = CarrierBus(name="grid", carrier="electricity",
+        ...                      excess_cost=2)
+        >>> electricity_bus.excess_cost
+        2
+        >>> heat_bus = CarrierBus(name="heating", carrier="heat")
+        >>> heat_bus
+        <CarrierBus 'heating', carrier='heat', shortage: None, excess: None>
+        >>> h2_bus = CarrierBus(name="h2_network", carrier="hydrogen",
+        ...                     excess_cost=0, shortage_cost=99)
         >>> h2_bus
-        <CarrierBus 'h2_network' carrier='hydrogen'>
+        <CarrierBus 'h2_network', carrier='hydrogen', shortage: 99, excess: 0>
         """
-        super().__init__(label=name)
+        super().__init__(label=name, balanced=balanced)
         self.carrier = carrier
         self.name = name
+        self.excess_cost = excess_cost
+        self.shortage_cost = shortage_cost
+
+        if excess_cost is not None:
+            self.subnode(
+                Sink,
+                local_name="excess",
+                inputs={
+                    self: Flow(
+                        variable_costs=excess_cost,
+                    )
+                },
+            )
+
+        if shortage_cost is not None:
+            self.subnode(
+                Source,
+                local_name="shortage",
+                outputs={
+                    self: Flow(
+                        variable_costs=shortage_cost,
+                    )
+                },
+            )
 
     def __repr__(self):
-        return f"<CarrierBus '{self.name}' carrier='{self.carrier}'>"
+        return (
+            f"<CarrierBus '{self.name}', carrier='{self.carrier}', "
+            f"shortage: {self.shortage_cost}, excess: {self.excess_cost}>"
+        )

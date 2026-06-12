e2ec7e144686075723367aed4266893133e82b51
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-06-12 22:02:53 +0200

    Test as if tespy would not exist.

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,3 +1,5 @@
+import importlib
+import sys
 from unittest.mock import patch
 
 import pytest
@@ -6,6 +8,24 @@ from oemof.eesyplan.importer import cop
 
 
 def test_tespy_not_installed():
+    """Set Network to None."""
     with patch.object(cop, "Network", None):
+        with pytest.raises(ModuleNotFoundError, match="TESPy is required"):
+            cop.calculate_cop_tespy(10, 40)
+
+
+def test_tespy_not_installed_via_import():
+    """As if tespy would not exist."""
+    with patch.dict(
+        sys.modules,
+        {
+            "tespy": None,
+            "tespy.components": None,
+            "tespy.connections": None,
+            "tespy.networks": None,
+        },
+    ):
+        importlib.reload(cop)
+
         with pytest.raises(ModuleNotFoundError, match="TESPy is required"):
             cop.calculate_cop_tespy(10, 40)

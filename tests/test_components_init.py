d9f469c452cce522c5ce3b2de2d3fc9f435542d6
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-06-22 14:37:30 +0200

    Add error test

--- tests/test_components_init.py
+++ tests/test_components_init.py
@@ -1,4 +1,9 @@
+import pytest
+
+from oemof.eesyplan.components.buses.carrier import CarrierBus as Bus
+from oemof.eesyplan.components.converters.Boiler import Boiler
 from oemof.eesyplan.components.transport.heat import HeatingNetwork
+from oemof.eesyplan.project import Project
 from oemof.solph.components import Sink
 
 
@@ -8,3 +13,37 @@ def test_heating_network():
     assert isinstance(hn._Node__subnodes[0], Sink)
     hn_wo_losses = HeatingNetwork(name="Heating Network")
     assert hn_wo_losses._Node__subnodes == []
+
+
+def test_capacity_null_in_dispatch_mode():
+    bus = Bus(name="bus")
+    # Investment
+    Boiler(
+        name="central_gas_boiler",
+        bus_in_fuel=bus,
+        bus_out_heat=bus,
+        age_installed=0,
+        capex_var=1000,
+        opex_fix=1000,
+        lifetime=20,
+        maximum_capacity=None,
+        efficiency=0.8,
+        opex_var=0,
+        optimize_cap=True,
+        project_data=Project(
+            name="Project_X", lifetime=20, tax=0, discount_factor=0.01
+        ),
+    )
+    # dispatch
+    msg = "An installed capacity of 0 is not valid for dispatch mode."
+    with pytest.raises(ValueError, match=msg):
+        Boiler(
+            name="central_gas_boiler",
+            bus_in_fuel=bus,
+            bus_out_heat=bus,
+            opex_var=0,
+            optimize_cap=False,
+            project_data=Project(
+                name="Project_X", lifetime=20, tax=0, discount_factor=0.01
+            ),
+        )

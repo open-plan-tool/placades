827e1ed4847edb5c0af25ef82be8b960cf77bb16
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-07-02 20:47:50 +0200

    Add zip test

--- tests/test_datapackage.py
+++ tests/test_datapackage.py
@@ -1,6 +1,9 @@
 import shutil
 import warnings
+import zipfile
 from pathlib import Path
+
+import pytest
 
 from oemof.datapackage import datapackage  # noqa
 from oemof.eesyplan import import_results
@@ -22,3 +25,23 @@ def test_simple_datapackage():
     import_results(path=result_path, es=es.create_energy_system_from_dp(path))
     shutil.rmtree(result_path)
     assert ~result_path.exists()
+
+
+def test_zip_datapackage():
+    dir_name = Path(Path(__file__).parent, "test_data", "openPlan_package")
+    filename = Path(Path(__file__).parent, "test_data", "openPlan_package")
+    shutil.make_archive(str(filename), "zip", dir_name)
+    with warnings.catch_warnings():
+        warnings.simplefilter(action="ignore", category=FutureWarning)
+        result_path = Path(Path.home(), ".oemof", "test_eesyplan_567263FG")
+        es.solve_energy_system_from_dp(filename, results_path=result_path)
+        es.solve_energy_system_from_dp(
+            filename.with_suffix(".zip"), plot="graph"
+        )
+        mzip = zipfile.ZipFile(filename.with_suffix(".zip"), "a")
+        mzip.write(Path(Path(__file__).parent, "test_data", "dp2.json"))
+        mzip.close()
+    with pytest.raises(ValueError, match="To many json files"):
+        es.solve_energy_system_from_dp(filename.with_suffix(".zip"))
+    Path(dir_name.parent, "openPlan_package.graphml").unlink()
+    filename.with_suffix(".zip").unlink()

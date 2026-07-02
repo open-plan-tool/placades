6e82f797b86bf05f3a857aa174f31d4e4bdeb685
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-07-02 14:13:06 +0200

    Rename storage modules to pep8 conventions

--- src/oemof/eesyplan/__init__.py
+++ src/oemof/eesyplan/__init__.py
@@ -37,12 +37,12 @@ from oemof.eesyplan.components.providers.DSO_electricity import DsoElectricity
 from oemof.eesyplan.components.providers.DSO_fuel import DsoFuel
 from oemof.eesyplan.components.providers.DSO_heat import DsoHeat
 from oemof.eesyplan.components.providers.DSO_hydrogen import DsoHydrogen
-from oemof.eesyplan.components.storages.ElectricalStorage import (
+from oemof.eesyplan.components.storages.electrical_storage import (
     ElectricalStorage,
 )
-from oemof.eesyplan.components.storages.FuelStorage import FuelStorage
-from oemof.eesyplan.components.storages.HydrogenStorage import HydrogenStorage
-from oemof.eesyplan.components.storages.ThermalStorage import ThermalStorage
+from oemof.eesyplan.components.storages.fuel_storage import FuelStorage
+from oemof.eesyplan.components.storages.hydrogen_storage import HydrogenStorage
+from oemof.eesyplan.components.storages.thermal_storage import ThermalStorage
 from oemof.eesyplan.datapackage.results import export_results
 from oemof.eesyplan.datapackage.results import import_results
 from oemof.eesyplan.model import EnergySystem

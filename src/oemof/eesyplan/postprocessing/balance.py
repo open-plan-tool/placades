519e982124b3da8907313596d5b02d19d2591435
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-07-02 13:19:26 +0200

    Fix node balance and add aggregation

--- src/oemof/eesyplan/postprocessing/balance.py
+++ src/oemof/eesyplan/postprocessing/balance.py
@@ -9,13 +9,14 @@ from oemof.tools.debugging import ExperimentalFeatureWarning
 warnings.filterwarnings("ignore", category=ExperimentalFeatureWarning)
 
 
-def nodes_io(flows):
+def nodes_io(flows, aggregate=False):
     """
     Create an i/o-balance around every node.
 
     Parameters
     ----------
     flows : pandas.DataFrame
+    aggregate : bool
 
     Returns
     -------
@@ -23,20 +24,33 @@ def nodes_io(flows):
 
     """
     logging.info("Process results")
+
+    if aggregate:
+        levels = [0, 1]
+        l_keys = ["in", "out"]
+    else:
+        levels = [0, 1, 2]
+        l_keys = ["from", "to"]
+
     nodes = {b[0] for b in flows.columns} | {b[1] for b in flows.columns}
 
     balances = {}
     for node in nodes:
-        in_flow = flows[[c for c in flows.columns if c[1] == node]]
-        out_flow = flows[[c for c in flows.columns if c[0] == node]]
-        balances[node] = pd.concat(
-            [in_flow, out_flow], keys=["in", "out"], axis=1
-        )
+        try:
+            in_flow = flows.xs(node, level=1, axis=1)
+        except KeyError:
+            in_flow = pd.DataFrame(index=flows.index)
+        try:
+            out_flow = flows[node]
+        except KeyError:
+            out_flow = pd.DataFrame(index=flows.index)
+        balances[node] = pd.concat([in_flow, out_flow], keys=l_keys, axis=1)
+
     return (
         pd.DataFrame(
             pd.concat(balances.values(), keys=balances.keys(), axis=1)
         )
-        .T.groupby(level=[0, 1])
+        .T.groupby(level=levels)
         .sum()
         .T
     )

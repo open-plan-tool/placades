b65f545d83292c6bf4e6ac166eca5d905628f958
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2026-07-06 12:39:28 +0200

    Simplyfy example code

--- examples/simple_datapackage/simple_datapackage.py
+++ examples/simple_datapackage/simple_datapackage.py
@@ -3,61 +3,32 @@ import warnings
 from pathlib import Path
 
 from oemof.datapackage import datapackage  # noqa
-from oemof.eesyplan import TYPEMAP
-from oemof.eesyplan import EnergySystem
 from oemof.eesyplan import export_results
 from oemof.eesyplan import import_results
-from oemof.eesyplan.model import optimise
-from oemof.network import graph
+from oemof.eesyplan.datapackage.energy_system import (
+    create_energy_system_from_dp,
+)
+from oemof.eesyplan.datapackage.energy_system import (
+    solve_energy_system_from_dp,
+)
 from oemof.tools.debugging import ExperimentalFeatureWarning
 from oemof.tools.logger import define_logging
-from oemof.visio import ESGraphRenderer
 
 warnings.filterwarnings("ignore", category=ExperimentalFeatureWarning)
 
 
-def create_energy_system_from_dp():
-    results_path = Path(Path.home(), "eesyplan", "results")
-    scenario_name = "test_placade_example"
-    scenario_dir = "openPlan_package"
+def main(debug=False):
+    scenario_dir = Path("openPlan_package")
     plot = "graph"  # "graph", "visio", None
 
-    Path.mkdir(results_path, parents=True, exist_ok=True)
+    results = solve_energy_system_from_dp(path=scenario_dir, plot=plot)
 
-    # create energy system object from the datapackage
-    es = EnergySystem.from_datapackage(
-        Path(scenario_dir, "datapackage.json"),
-        attributemap={},
-        typemap=TYPEMAP,
-    )
-
-    if plot == "graph":
-        graph.create_nx_graph(
-            es, filename=Path(results_path, "test_graph.graphml")
-        )
-    elif plot == "visio":
-        energy_system_graph = Path(
-            results_path, f"{scenario_name}_energy_system.png"
-        )
-
-        es_graph = ESGraphRenderer(
-            es,
-            legend=False,
-            filepath=str(energy_system_graph),
-            img_format="png",
-        )
-        es_graph.render()
-    return es
-
-
-def main(debug=False):
-    results = optimise(energy_system=create_energy_system_from_dp())
     print("'*************** First time **************")
     process_results(results)  # original result object
     results_path = Path(Path.home(), "openplan", "openPlan_results")
     results_path.mkdir(parents=True, exist_ok=True)
     export_results(results, path=results_path)
-    es = create_energy_system_from_dp()
+    es = create_energy_system_from_dp(path=scenario_dir)
     results = import_results(results_path, es=es)
     print("'*************** Second time **************")
     process_results(results)  # imported result object

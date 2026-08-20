542e6babe66894abb2c3e3064285db32d480494f
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-08-20 23:19:52 +0200

    Clean investment module and add tests

--- src/oemof/eesyplan/investment.py
+++ src/oemof/eesyplan/investment.py
@@ -21,6 +21,11 @@ def crf(duration, discount_factor):
     float : CRF capital recovery factor, used to convert a one-time
     investment into an equivalent annual cost over a given duration
 
+    Examples
+    --------
+    >>> round(crf(10, 0.05), 5)
+    0.1295
+
     """
     if discount_factor != 0:
         vcrf = (discount_factor * (1 + discount_factor) ** duration) / (
@@ -32,79 +37,31 @@ def crf(duration, discount_factor):
     return vcrf
 
 
-def get_replacement_costs(  # todo Function not needed anymore, CRF-Method is consistent and no extra calculations needed (delete at some point)
-    age_of_asset,
-    project_lifetime,
-    asset_lifetime,
-    first_time_investment,
-    discount_factor,
-):
-    """From mvs src/multi_vector_simulator/C2_economic_functions.py"""
-    if project_lifetime + age_of_asset == asset_lifetime:
-        number_of_investments = 1
-    else:
-        number_of_investments = round(
-            (project_lifetime + age_of_asset) / asset_lifetime + 0.5
-        )
-
-    replacement_costs = 0
-    latest_investment = first_time_investment
-    year = -age_of_asset
-    if abs(year) > asset_lifetime:
-        warnings.warn(
-            f"The age of the asset ({age_of_asset} years) is lower or equal "
-            f"than the asset lifetime ({asset_lifetime} years). This does not "
-            f"make sense, as a replacement is imminent or should already have "
-            f"happened. Please check this value.",
-            stacklevel=2,
-        )
-
-    present_value_of_capital_expenditures = pd.DataFrame(
-        [0.0 for _i in range(project_lifetime + 1)],
-        index=list(range(project_lifetime + 1)),
-    )
-
-    for _count_of_replacements in range(1, number_of_investments):
-        year += asset_lifetime
-        if year < project_lifetime:
-            latest_investment = first_time_investment / (
-                (1 + discount_factor) ** year
-            )
-            replacement_costs += latest_investment
-            present_value_of_capital_expenditures.loc[year] = latest_investment
-        elif year == project_lifetime:
-            warnings.warn(
-                "No asset replacement costs are computed for the project's "
-                "last year as the asset reach its end-of-life exactly on that"
-                " year",
-                stacklevel=2,
-            )
-
-    if year != project_lifetime:
-        year += asset_lifetime
-    if year > project_lifetime:
-        linear_depreciation_last_investment = (
-            latest_investment / asset_lifetime
-        )
-        value_at_project_end = (
-            linear_depreciation_last_investment
-            * (year - project_lifetime)
-            / (1 + discount_factor) ** project_lifetime
-        )
-        replacement_costs -= value_at_project_end
-        present_value_of_capital_expenditures.loc[
-            project_lifetime
-        ] = -value_at_project_end
-
-    return replacement_costs
-
-
-def calculate_annuity(  # Todo Costs of existing components will be calculated in postprocessing
+def calculate_annuity(
     capex_spec,
     asset_lifetime,
     tax,
     discount_factor,
 ):
+    """
+    Calculates the annuity.
+
+    Parameters
+    ----------
+    capex_spec
+    asset_lifetime
+    tax
+    discount_factor
+
+    Returns
+    -------
+
+    Examples
+    --------
+    >>> round(calculate_annuity(100, 15, 0.01, 0.05), 5)
+    9.73057
+
+    """
     npv = capex_spec * (1 + tax)
 
     annuity = npv * crf(asset_lifetime, discount_factor)
